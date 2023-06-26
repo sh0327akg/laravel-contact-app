a817316c55fc0f94a5bba50f4cd4ded2d8fce062
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-06-26 12:25:20 +0900

    [add]Contactコントローラーを実装する

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ContactServiceInterface;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\ContactRequest;
 
 class ContactController extends Controller
 {
-    //
+    private ContactServiceInterface $contactService;
+
+    public function __construct(ContactServiceInterface $contactService)
+    {
+        $this->contactService = $contactService;
+    }
+
+    public function index()
+    {   
+        $contacts = $this->contactService->getContacts();
+        return view('contact.index',compact('contacts'));
+    }
+
+    public function create()
+    {
+        $departments = $this->contactService->getDepartments();
+        return view('contact.create', compact('departments'));
+    }
+
+    public function store(ContactRequest $request)
+    {
+        DB::transaction(function() use ($request){
+            $this->contactService->createContact(
+                $request->department_id,
+                $request->name,
+                $request->email,
+                $request->content,
+                (int)$request->age,
+                (int)$request->gender
+            );
+        });
+        return redirect()->route('contact.index');
+    }
 }
---
+++ src/app/Http/Requests/ContactRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'department_id' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'content' => ['required', 'string', 'max:1000'],
+            'age' => ['required', 'integer', 'min:1', 'max:100'],
+            'gender' => ['required', 'integer', Rule::in([1, 2, 3])],
+        ];
+    }
+}
