d5e0d107c5197ea90de380f7622170fb8ace3cab
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-06-27 16:39:25 +0900

    [add]性別を変換するメソッドを定義する

--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -14,6 +14,9 @@ class Contact extends Model
         return $this->belongsTo(Department::class);
     }
 
+    /**
+     * ホワイトリスト
+     */
     protected $fillable = [
         'department_id',
         'name',
@@ -22,4 +25,17 @@ class Contact extends Model
         'age',
         'gender',
     ];
+
+    /**
+     * 性別の値を文字列に変換
+     *
+     * @param int $value
+     * @return string|null
+     */
+    public function getGenderAttribute(int $value): ?string
+    {
+        $genders = [1 => '男性', 2 => '女性', 3 => '未解答'];
+
+        return $genders[$value] ?? null;
+    }
 }
